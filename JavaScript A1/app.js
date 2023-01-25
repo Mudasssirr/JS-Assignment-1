edefbce2bd7bc6792dd2de957a0095e4332f8bca
M. Mudassir Awan <121383917+Mudasssirr@users.noreply.github.com>
2023-01-25 14:56:01 +0500

    Java Script Assignment 1
    Chap 1 (Alert) Assignment

---
+++ JavaScript A1/app.js
@@ -0,0 +1,17 @@
+// Cahpter 1
+
+//Question 1
+//alert("Hello, Sir")
+
+//Question 2
+//alert("Error! Please enter a valid password")
+
+//Question 3
+//alert("Welcone to JS Land... \n Happy Coding!")
+
+//Question 4
+//alert("Welocme to JS Land...")
+//alert("Happy Coding!")
+
+//Question 5
+//alert("Hello... I can rum JS through my web browser's console")
